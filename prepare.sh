35fd9ab8cb1f4fafd2fe13dbdee63c7993853f0f
mipolak <misopo@gmail.com>
2020-10-22 12:22:14 +0000

    fixes

--- prepare.sh
+++ prepare.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 IMAGE="ubuntu-20.04.1-live-server-amd64.iso"
 MAINPWD=$(pwd)
+BPATH=${MAINPWD}/DATA/BUILD
 
 #check and install/configure dependecies
 [ $(dpkg -s isolinux 2>/dev/null | grep -c "ok installed") -eq 1 ] || sudo apt install --yes isolinux
@@ -10,21 +11,21 @@ MAINPWD=$(pwd)
 [ -d ./DATA ] || ( mkdir -p ./DATA/{ISOMOUNT,FINAL,BUILD/{squashfs,custom,cd}} )
 
 ##### filesystem.squashfs update & including latest patches into the image
-sudo mount -o loop ./SOURCEIMG/${IMAGE} ./DATA/ISOMOUNT
-rsync --exclude=/casper/filesystem.squashfs/ -a ./DATA/ISOMOUNT ./DATA/BUILD/cd 
+sudo mount -o loop ${MAINPWD}/SOURCEIMG/${IMAGE} ${MAINPWD}/DATA/ISOMOUNT
+rsync --exclude=/casper/filesystem.squashfs/ -a ${MAINPWD}/DATA/ISOMOUNT ${BPATH}/cd 
 sudo modprobe squashfs
-sudo mount -t squashfs -o loop ./DATA/ISOMOUNT/casper/filesystem.squashfs ./DATA/BUILD/squashfs
-sudo rsync -a ./DATA/BUILD/squashfs/ ./DATA/BUILD/custom
+sudo mount -t squashfs -o loop ${MAINPWD}/DATA/ISOMOUNT/casper/filesystem.squashfs ${BPATH}/squashfs
+sudo rsync -a ${BPATH}/squashfs/ ${BPATH}/custom
 
-# network data for chroot env
-sudo cp ./DATA/BUILD/custom/etc/resolv.conf ./DATA/BUILD/custom/etc/resolv.conf_bck
-sudo cp ./DATA/BUILD/custom/etc/hosts ./DATA/BUILD/custom/etc/hosts_bck
-sudo cp ./DATA/BUILD/custom/etc/apt/sources.list ./DATA/BUILD/custom/etc/apt/sources.list_bck 
-sudo cp /etc/resolv.conf /etc/hosts ./DATA/BUILD/custom/etc/
-sudo cp /etc/apt/sources.list ./DATA/BUILD/custom/etc/apt/
+# network comfgiuration for modification in chroot env
+sudo cp ${BPATH}/custom/etc/resolv.conf ${BPATH}/custom/etc/resolv.conf_bck
+sudo cp ${BPATH}/custom/etc/hosts ${BPATH}/custom/etc/hosts_bck
+sudo cp ${BPATH}/custom/etc/apt/sources.list ${BPATH}/custom/etc/apt/sources.list_bck 
+sudo cp /etc/resolv.conf /etc/hosts ${BPATH}/custom/etc/
+sudo cp /etc/apt/sources.list ${BPATH}/custom/etc/apt/
 
 # modification inside chroot
-sudo chroot ./DATA/BUILD/custom << EOT
+sudo chroot ${BPATH}/custom << EOT
 mount -t proc none /proc/
 mount -t sysfs none /sys/
 export HOME=/root
@@ -34,42 +35,40 @@ apt-get clean
 umount /proc
 umount /sys
 EOT
+#network configuration restored to default 
+sudo cp ${BPATH}/custom/etc/resolv.conf_bck ${BPATH}/custom/etc/resolv.conf 
+sudo cp ${BPATH}/custom/etc/hosts_bck ${BPATH}/custom/etc/hosts
+sudo cp ${BPATH}/custom/etc/apt/sources.list_bck ${BPATH}/custom/etc/apt/sources.list
 
-sudo cp ./DATA/BUILD/custom/etc/resolv.conf_bck ./DATA/BUILD/custom/etc/resolv.conf 
-sudo cp ./DATA/BUILD/custom/etc/hosts_bck ./DATA/BUILD/custom/etc/hosts
-sudo cp ./DATA/BUILD/custom/etc/apt/sources.list_bck ./DATA/BUILD/custom/etc/apt/sources.list
-
-sudo mv ./DATA/BUILD/cd/casper/filesystem.manifest ./DATA/BUILD/cd/casper/filesystem.manifest_old
-sudo chroot ./DATA/BUILD/custom dpkg-query -W --showformat='${Package} ${Version}\n' > ./DATA/BUILD/cd/casper/filesystem.manifest
-sudo mksquashfs ./DATA/BUILD/custom ./DATA/BUILD/cd/casper/filesystem.squashfs
-sudo rm ./DATA/BUILD/cd/md5sum.txt
+sudo mv ${BPATH}/cd/casper/filesystem.manifest ${BPATH}/cd/casper/filesystem.manifest_old
+sudo chroot ${BPATH}/custom dpkg-query -W --showformat='${Package} ${Version}\n' > ${BPATH}/cd/casper/filesystem.manifest
+sudo mksquashfs ${BPATH}/custom ${BPATH}/cd/casper/filesystem.squashfs
+sudo rm ${BPATH}/cd/md5sum.txt
 
 # copy cloud-init
-[ -d ./DATA/BUILD/cd/nocloud ] || sudo mkdir ./DATA/BUILD/cd/nocloud
-sudo cp configs/user-data ./DATA/BUILD/cd/nocloud/user-data
-sudo touch ./DATA/BUILD/cd/nocloud/meta-data
+[ -d ${BPATH}/cd/nocloud ] || sudo mkdir ${BPATH}/cd/nocloud
+sudo cp configs/user-data ${BPATH}/cd/nocloud/user-data
+sudo touch ${BPATH}/cd/nocloud/meta-data
 
 #
-sudo rm -rf './DATA/BUILD/cd/[BOOT]'
+sudo rm -rf '${BPATH}/cd/[BOOT]'
 
 # umount loops 
-sudo umount ${MAINPWD}/DATA/BUILD/squashfs
+sudo umount ${BPATH}/squashfs
 sudo umount ${MAINPWD}/DATA/ISOMOUNT
 
 # update boot flags
-sed -i 's|---|autoinstall ds=nocloud\\\;s=/cdrom/nocloud/ locale=en_US console-setup/ask_detect=false  console-setup/layoutcode=us console-setup/layoutcode=us console-setup/modelcode=SKIP translation/warn-light=true localechooser/translation/warn-severe=true keyboard-configuration/modelcode=SKIP keyboard-configuration/layout="English (US)" keyboard-configuration/variant="English (US)" ---|g' ${MAINPWD}/DATA/BUILD/cd/boot/grub/grub.cfg
-sed -i 's|---|autoinstall ds=nocloud;s=/cdrom/nocloud/ locale=en_US console-setup/ask_detect=false  console-setup/layoutcode=us console-setup/layoutcode=us console-setup/modelcode=SKIP translation/warn-light=true localechooser/translation/warn-severe=true keyboard-configuration/modelcode=SKIP keyboard-configuration/layout="English (US)" keyboard-configuration/variant="English (US)" ---|g' ${MAINPWD}/DATA/BUILD/cd/isolinux/txt.cfg
+sed -i 's|---|autoinstall ds=nocloud\\\;s=/cdrom/nocloud/ locale=en_US console-setup/ask_detect=false  console-setup/layoutcode=us console-setup/layoutcode=us console-setup/modelcode=SKIP translation/warn-light=true localechooser/translation/warn-severe=true keyboard-configuration/modelcode=SKIP keyboard-configuration/layout="English (US)" keyboard-configuration/variant="English (US)" ---|g' ${BPATH}/cd/boot/grub/grub.cfg
+sed -i 's|---|autoinstall ds=nocloud;s=/cdrom/nocloud/ locale=en_US console-setup/ask_detect=false  console-setup/layoutcode=us console-setup/layoutcode=us console-setup/modelcode=SKIP translation/warn-light=true localechooser/translation/warn-severe=true keyboard-configuration/modelcode=SKIP keyboard-configuration/layout="English (US)" keyboard-configuration/variant="English (US)" ---|g' ${BPATH}/cd/isolinux/txt.cfg
 
-cd ${MAINPWD}
-
-
+#recreate md5sum
 sudo -s << EOR
-cd ./DATA/BUILD/cd
+cd ${BPATH}/cd
 find . -type f -print0 | xargs -0 md5sum > md5sum.txt
 EOR
 
 # create image
-cd ${MAINPWD}/DATA/BUILD/cd
+cd ${MAINPWD${BPATH}/cd
 xorriso -as mkisofs -r \
   -V "Ubuntu Server 20.04 custom amd64" -o ${MAINPWD}/DATA/FINAL/ubuntu-20.04.1-live-server-amd64-autoinstall.iso \
   -J -l -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table \
